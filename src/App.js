ba87327992ba32a58aae3af2863c429979be8e8f
KarlaOrtega200418 <168759168+KarlaOrtega200418@users.noreply.github.com>
2024-05-02 22:33:27 -0600

    El de Lupita

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register.js';
 import ARComponents from './components/ARComponents';
 import { Projects2 } from './components/Project2.js';
+import { deflate } from 'three/examples/jsm/libs/fflate.module.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { NavBar } from './components/NavBar.js';
+
+
+
 
 function App() {
   const [userId, setUserId] = useState(null);
@@ -19,12 +23,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route path = '/' element = {<Login/>}></Route> */}
         <Route path='/' element={<Login onLoginSuccess={handleLoginSuccess} />} />
-        {/* Pass userId to Projects2 component */}
+        <Route path = '/register' element = {<Register/>}></Route>
+        <Route path = '/navbar' element = {<NavBar/>}></Route>
+        {/* <Route path = '/projects' element = {<Projects2/>}></Route> */}
+        <Route path="/ar" element={<ARComponents />}></Route>
+        <Route path="/ar/:id_objeto" element={<ARComponents />}></Route>
         <Route path='/projects/:userId' element={<Projects2 />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/navbar' element={<NavBar />} />
-        <Route path="/ar" element={<ARComponents />} />
+
       </Routes>
     </BrowserRouter>
   );
